0dfb78a8c6d8d458995a5221d82d0194e9d6074d
jenfourla <56540178+jenfourla@users.noreply.github.com>
2020-03-23 14:05:54 +0200

    Add files via upload
    den trexei h anadromh

--- count_fixed_polyominoes.py
+++ count_fixed_polyominoes.py
@@ -1,16 +1,63 @@
 import sys
 import pprint
 import argparse
+sys.setrecursionlimit(10**6)
+
 g={}
 neighbours=[]
 nodes=[]
 h=[]
-
-
-n=sys.argv[0]
+if (sys.argv[1]=="-p"):
+    b=sys.argv[2]
+    n=int(b)
+    u=n-1
+else:
+    b=sys.argv[1]
+    n=int(b)
+    u=n-1
 metrhths_gr=0
 k=1 
-u=n-1
+
+def CountFixedPolyominoes(G,untried,n,p,c):
+    
+    while(not(len(untried)==0)):
+        found = True
+        print("the value of untried is: ", untried)
+        u = untried.pop()
+        print("the NEW VALUE of untried is: ", untried)
+        
+        
+        untried1=tuple(untried)
+        u1 = tuple(u)
+        p.append(u)
+        p1 = tuple(p)
+        if (len(p)==n):
+             c=c+1
+        else:
+            
+            new_neighbors = []
+            
+            for v in G[u1]:
+                
+                if  (((p1.count(v)==0))and(not(untried1.__contains__(v)))and(found)):
+                    for t in p:
+                        
+                        if (t!=u):
+                            
+                            for j in G[t]:
+                                if (v==j):
+                                    found=False
+                                else:
+                                    new_neighbors.append(v)
+                        else:
+                            
+                            new_neighbors.append(v)
+                        
+            new_untried = untried + new_neighbors
+            
+            CountFixedPolyominoes(G,new_untried,n,p,c)
+        p.remove(u)
+    return c
 for i in range(-n+2,n):
     if(i<0):
         for j in range(1,k+1):
@@ -48,19 +95,29 @@ for l in nodes:
         y=[l[0],l[1]-1]
         neighbours.append(y)
     for y in neighbours:
-        print(l[0])
+        
         if (metrhths_gr==0):
             d2 = {(l[0],l[1]):[y]}
             g.update(d2)
             
             metrhths_gr=metrhths_gr+1
         else:
-            print((l[0],l[1]))
+            
             d1= {(l[0],l[1]):[g[(l[0],l[1])],y]}
             g.update(d1)
     neighbours.clear()     
     metrhths_gr=0
+
+if (sys.argv[1]=="-p"):
+    print(g)
+    print(CountFixedPolyominoes(g,[(0,0)], n,[],0))
+else:
+    print(CountFixedPolyominoes(g,[(0,0)], n,[],0))    
  
+
+
+   
+
  
 
         
